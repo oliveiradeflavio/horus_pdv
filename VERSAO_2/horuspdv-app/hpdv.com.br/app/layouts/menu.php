eb4e1203c808df48f9e599ce0c169ec3bf0a28a4
Flávio Oliveira <oliveiradeflavio@gmail.com>
2024-02-20 10:53:33 -0300

    implementando o menu offcanvas

---
+++ VERSAO_2/horuspdv-app/hpdv.com.br/app/layouts/menu.php
@@ -0,0 +1,13 @@
+<nav>
+    <div id='menu'>
+        <button class="btn btn-enter" type="button" data-bs-toggle="offcanvas" data-bs-target="#menuSuperior" aria-controls="offcanvasTop">Menu</button>
+    </div>
+    <div class="offcanvas offcanvas-top" tabindex="-1" id="menuSuperior" aria-labelledby="offcanvasTopLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            ...
+        </div>
+    </div>
--- VERSAO_2/horuspdv-app/hpdv.com.br/app/web/home.php
+++ VERSAO_2/horuspdv-app/hpdv.com.br/app/web/home.php
@@ -8,50 +8,52 @@
 </head>
 
 <body>
+    <?php require '../layouts/menu.php' ?>
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 center">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4 card-box ">
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 center">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-md-4 card-box ">
 
-                                <a href="#">
-                                    <i class="fa-solid fa-user-plus"></i>
-                                    Cadastro de Cliente</a>
+                                    <a href="#">
+                                        <i class="fa-solid fa-user-plus"></i>
+                                        Cadastro de Cliente</a>
+                                </div>
+                                <div class="col-md-4 card-box ">
+                                    <a href="#">
+                                        <i class="fa-solid fa-tags"></i>
+                                        Cadastro de Produto</a>
+                                </div>
+                                <div class="col-md-4 card-box ">
+                                    <a href="#">
+                                        <i class="fa-solid fa-truck-fast"></i>
+                                        Cadastro de Fornecedor</a>
+                                </div>
                             </div>
-                            <div class="col-md-4 card-box ">
-                                <a href="#">
-                                    <i class="fa-solid fa-tags"></i>
-                                    Cadastro de Produto</a>
-                            </div>
-                            <div class="col-md-4 card-box ">
-                                <a href="#">
-                                    <i class="fa-solid fa-truck-fast"></i>
-                                    Cadastro de Fornecedor</a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4 card-box ">
-                                <a href="#">
-                                    <i class="fa-solid fa-clock-rotate-left"></i>
-                                    Histórico de Vendas</a>
-                            </div>
-                            <div class="col-md-4 card-box ">
-                                <a href="#">
-                                    <i class="fa-solid fa-file-invoice"></i>
-                                    Relatórios</a>
-                            </div>
-                            <div class="col-md-4 card-box ">
-                                <a href="#">
-                                    <i class="fa-solid fa-cart-arrow-down"></i>
-                                    Iniciar Vendas</a>
+                            <div class="row">
+                                <div class="col-md-4 card-box ">
+                                    <a href="#">
+                                        <i class="fa-solid fa-clock-rotate-left"></i>
+                                        Histórico de Vendas</a>
+                                </div>
+                                <div class="col-md-4 card-box ">
+                                    <a href="#">
+                                        <i class="fa-solid fa-file-invoice"></i>
+                                        Relatórios</a>
+                                </div>
+                                <div class="col-md-4 card-box ">
+                                    <a href="#">
+                                        <i class="fa-solid fa-cart-arrow-down"></i>
+                                        Iniciar Vendas</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
 
