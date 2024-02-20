68d23aa7f96d7353a0ce37b37b2fd5b3671194d6
Flávio Oliveira <oliveiradeflavio@gmail.com>
2024-02-19 21:11:20 -0300

    implementado tela de home com menus de acesso

--- VERSAO_2/horuspdv-app/hpdv.com.br/app/web/home.php
+++ VERSAO_2/horuspdv-app/hpdv.com.br/app/web/home.php
@@ -9,19 +9,64 @@
 
 <body>
     <main>
-        <section id="container-home">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card card-home">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4 card-box ">
 
+                                <a href="#">
+                                    <i class="fa-solid fa-user-plus"></i>
+                                    Cadastro de Cliente</a>
+                            </div>
+                            <div class="col-md-4 card-box ">
+                                <a href="#">
+                                    <i class="fa-solid fa-tags"></i>
+                                    Cadastro de Produto</a>
+                            </div>
+                            <div class="col-md-4 card-box ">
+                                <a href="#">
+                                    <i class="fa-solid fa-truck-fast"></i>
+                                    Cadastro de Fornecedor</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 card-box ">
+                                <a href="#">
+                                    <i class="fa-solid fa-clock-rotate-left"></i>
+                                    Histórico de Vendas</a>
+                            </div>
+                            <div class="col-md-4 card-box ">
+                                <a href="#">
+                                    <i class="fa-solid fa-file-invoice"></i>
+                                    Relatórios</a>
+                            </div>
+                            <div class="col-md-4 card-box ">
+                                <a href="#">
+                                    <i class="fa-solid fa-cart-arrow-down"></i>
+                                    Iniciar Vendas</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+
+
+
         </section>
+
+
 
     </main>
 </body>
 
 </html>
+
+<!-- cad cliente'
+cad produto'
+cad fornecedor'
+historico de vendas
+Relatórios
+Iniciar Vendas -->
