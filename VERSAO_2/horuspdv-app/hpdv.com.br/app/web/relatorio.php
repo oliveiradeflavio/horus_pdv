8ee9fec989eb105eac05e9b9f488591cef7bf88a
Flávio Oliveira <oliveiradeflavio@gmail.com>
2024-02-29 16:43:59 -0300

    Front da página relatorio finalizada

--- VERSAO_2/horuspdv-app/hpdv.com.br/app/web/relatorio.php
+++ VERSAO_2/horuspdv-app/hpdv.com.br/app/web/relatorio.php
@@ -19,39 +19,48 @@
                     <div class="card">
                         <ul id='tab_relatorio' class="nav nav-tabs nav-pills nav-fill">
                             <li class="nav-item">
-                                <a href="#tab_relatorio_cliente" class="active"> <i class="fa-solid fa-user-plus"></i>
+                                <a class="active" data-bs-toggle="pill" data-bs-target="#tab_relatorio_cliente"> <i class="fa-solid fa-user-plus"></i>
                                     Relatório de Cliente
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="#tab_relatorio_produto"> <i class="fa-solid fa-tags"></i>
+                                <a data-bs-toggle="pill" data-bs-target="#tab_relatorio_produto"> <i class="fa-solid fa-tags"></i>
                                     Relatório de Produto</a>
                             </li>
                             <li class="nav-item">
 
-                                <a href="#tab_relatorio_fornecedor"> <i class="fa-solid fa-truck-fast"></i>
+                                <a data-bs-toggle="pill" data-bs-target="#tab_relatorio_fornecedor"> <i class="fa-solid fa-truck-fast"></i>
                                     Relatório de Fornecedor</a>
                             </li>
                             <li class="nav-item">
 
-                                <a href="#tab_historico_vendas"> <i class="fa-solid fa-clock-rotate-left"></i>
+                                <a data-bs-toggle="pill" data-bs-target="#tab_historico_vendas"> <i class=" fa-solid fa-clock-rotate-left"></i>
                                     Histórico de Vendas</a>
                             </li>
                         </ul>
                         <div id="tab_relatorio_content" class="tab-content">
-                            <div id="tab_relatorio_cliente" class="tab-pane fade show active" role="tabpanel">
+                            <div id="tab_relatorio_cliente" class="tab-pane fade active show" role="tabpanel">
                                 <div>
                                     <form action="#" method="post">
                                         <input type="hidden" name="csrf_token">
-
-                                        <div class="form-group">
-                                            <label class="switch" title="Ative para habilitar os campos">
-                                                <input type="checkbox" id="chb_relatorio_cliente">
-                                                <span class="slider">
-                                                </span>
-                                            </label>
+                                        <div class="form-group mt-3 mb-3">
+                                            <label for="">Tipo de Relatório</label>
+                                            <select name="tipo_relatorio" id="tipo_relatorio" class="form-select">
+                                                <option value="" selected>Selecionar</option>
+                                                <option value="pdf">PDF</option>
+                                                <option value="excel">Excel</option>
+                                            </select>
                                         </div>
+                                        <div class="form-group mt-3 mb-3">
+                                            <label for="">Período</label>
+                                            <select name="periodo_relatorio" id="periodo_relatorio" class="form-select">
+                                                <option value="" selected>Selecionar</option>
 
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <button type="submit" class="btn btn-primary btn-lg">Gerar Relatório</button>
+                                        </div>
                                     </form>
                                 </div>
                             </div>
@@ -60,13 +69,28 @@
                                     <form action="#" method="post">
                                         <input type="hidden" name="csrf_token">
 
-                                        <div class="form-group">
-                                            <label for="" class="switch" title="Ative para habilitar os campos">
-                                                <input type="checkbox" id="chb_relatorio_produto">
-                                                <span class="slider">
+                                        <div>
+                                            <form action="#" method="post">
+                                                <input type="hidden" name="csrf_token">
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Tipo de Relatório</label>
+                                                    <select name="tipo_relatorio" id="tipo_relatorio" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
+                                                        <option value="pdf">PDF</option>
+                                                        <option value="excel">Excel</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Período</label>
+                                                    <select name="periodo_relatorio" id="periodo_relatorio" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
 
-                                                </span>
-                                            </label>
+                                                    </select>
+                                                </div>
+                                                <div>
+                                                    <button type="submit" class="btn btn-primary btn-lg">Gerar Relatório</button>
+                                                </div>
+                                            </form>
                                         </div>
 
                                     </form>
@@ -78,13 +102,29 @@
                                     <form action="#" method="post">
                                         <input type="hidden" name="csrf_token">
 
-                                        <div class="form-group">
-                                            <label for="" class="switch" title="Ative para habilitar os campos">
-                                                <input type="checkbox" id="chb_relatorio_fornecedor">
-                                                <span class="slider">
+                                        <div>
+                                            <form action="#" method="post">
+                                                <input type="hidden" name="csrf_token">
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Tipo de Relatório</label>
+                                                    <select name="tipo_relatorio" id="tipo_relatorio" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
+                                                        <option value="pdf">PDF</option>
+                                                        <option value="excel">Excel</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Período</label>
+                                                    <select name="periodo_relatorio" id="periodo_relatorio" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
 
-                                                </span>
-                                            </label>
+                                                    </select>
+                                                </div>
+                                                <div>
+                                                    <button type="submit" class="btn btn-primary btn-lg">Gerar Relatório</button>
+                                                </div>
+                                            </form>
+
                                         </div>
 
                                     </form>
@@ -95,13 +135,35 @@
                                     <form action="#" method="post">
                                         <input type="hidden" name="csrf_token">
 
-                                        <div class="form-group">
-                                            <label for="" class="switch" title="Ative para habilitar os campos">
-                                                <input type="checkbox" id="chb_historico_vendas">
-                                                <span class="slider">
+                                        <div>
+                                            <form action="#" method="post">
+                                                <input type="hidden" name="csrf_token">
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Tipo de Relatório</label>
+                                                    <select name="tipo_relatorio" id="tipo_relatorio" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
+                                                        <option value="pdf">PDF</option>
+                                                        <option value="excel">Excel</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Opção de Pagamento</label>
+                                                    <select name="opcao_pagamento" id="opcao_pagamento" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
 
-                                                </span>
-                                            </label>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group mt-3 mb-3">
+                                                    <label for="">Período</label>
+                                                    <select name="periodo_relatorio" id="periodo_relatorio" class="form-select">
+                                                        <option value="" selected>Selecionar</option>
+
+                                                    </select>
+                                                </div>
+                                                <div>
+                                                    <button type="submit" class="btn btn-primary btn-lg">Gerar Relatório</button>
+                                                </div>
+                                            </form>
                                         </div>
                                     </form>
                                 </div>
