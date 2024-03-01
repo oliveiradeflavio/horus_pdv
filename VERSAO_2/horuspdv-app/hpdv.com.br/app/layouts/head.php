22cf7b34505ca0fa878c6b47348c75ded865a001
Flávio Oliveira <oliveiradeflavio@gmail.com>
2024-03-01 10:41:34 -0300

    Renomeado o arquivo css master.css para global.ss. Ajustado alguns parametros css da página de recuperação de senha para versão mobile.

--- VERSAO_2/horuspdv-app/hpdv.com.br/app/layouts/head.php
+++ VERSAO_2/horuspdv-app/hpdv.com.br/app/layouts/head.php
@@ -3,7 +3,7 @@
  <meta name="apple-mobile-web-app-capable" content="yes">
  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
  <meta name="theme-color" content="#211D3A">
- <meta name="application-name" content="Hórus PDV">
+ <meta name="application-name" content="PDV">
  <meta name="description" content="App para cadastro e venda de produtos">
  <meta name="author" content="Flávio Oliveira">
 
